8bc5e9679c4d3f4ef5bd78f261614323724d3c9d
Michael Paolo Cariaso <michael.cariaso@whitecloak.com>
2018-04-06 15:09:11 +0800

    Created dev branch.

--- src/main/java/com/shipserv/hr/web/rest/UserLoginServiceClientImpl.java
+++ src/main/java/com/shipserv/hr/web/rest/UserLoginServiceClientImpl.java
@@ -9,55 +9,55 @@ import com.shipserv.hr.web.domain.UserToken;
 
 @Service
 public class UserLoginServiceClientImpl implements UserLoginServiceClient {
-	
+
 	private String BASE_SERVICE_URL = "http://localhost";
 	private String SERVICE_CONTEXT_URI = "/hr-service";
 	private int BASE_SERVICE_PORT = 8080;
 	private String OAUTH2_LOGIN_URL = "/oauth/token";
-		
+
 	private String CLIENT_ID = "test01-id";
-	
+
 	private String CLIENT_SECRET = "test01-secretkey";
-	
+
 	private String GRANT_TYPE = "password";
-	
+
 	private String SCOPE = "read";
-	
+
 	/*formParam("client_id", "test01-id").
-	formParam("client_secret", "test01-secretkey").		
+	formParam("client_secret", "test01-secretkey").
 	formParam("grant_type", "password").
 	formParam("scope", "read").*/
-	
+
 	/*returns access token*/
 	public UserToken login(String userName, String password) {
-		
+
 		ValidatableResponse response =
-				given().					
+				given().
 					formParam("username", userName).
 					formParam("password", password).
 					formParam("client_id", CLIENT_ID).
-					formParam("client_secret", CLIENT_SECRET).		
+					formParam("client_secret", CLIENT_SECRET).
 					formParam("grant_type", GRANT_TYPE).
 					formParam("scope", SCOPE).
 					baseUri(BASE_SERVICE_URL).
 					port(BASE_SERVICE_PORT).
 					log().all(). //logs the request
-											
-				when().					
+
+				when().
 					post(SERVICE_CONTEXT_URI + OAUTH2_LOGIN_URL).
 				then().
 					log().all(); //logs the response
-				
-				
+
+
 				String accessToken = response.extract().body().jsonPath().get("access_token");
-				
+
 				UserToken userToken = new UserToken();
 				userToken.setUserName(userName);
 				userToken.setAccessToken(accessToken);
 				userToken.setFullName(userName);
-				
+
 				return userToken;
-		
+
 	}
 
 }
